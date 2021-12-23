05584b53d627f839135fd001b67cb64d39009d48
Jorge Luis <jorgeluis21.duran@gmail.com>
2021-12-23 10:25:50 -0400

    adding public folder to root-dir

--- src/app.ts
+++ src/app.ts
@@ -50,6 +50,7 @@ app.use(passport.session())
 app.use(`/${process.env['SUFIX_PATHNAME']}`, router )
 
 
-app.use('/uploads', express.static(path.resolve('uploads')))
+app.use('/uploads', express.static(path.resolve('uploads')));
+app.use('/public', express.static(path.join(__dirname, "public"));
 
 
